65bc62009159fc00b45dccbab05303a984d8651d
lupusruber <petarpro234@gmail.com>
2024-09-02 11:53:54 +0200

    changed consumer logic

--- scripts/consumer.py
+++ scripts/consumer.py
@@ -1,25 +1,35 @@
 from quixstreams import Application
-from pprint import pprint
 import json
-import  pandas as pd
+import pandas as pd
+
+
+list_of_topics = [
+    "auth_events",
+    "listen_events",
+    "page_view_events",
+    "status_change_events",
+]
 
 app = Application(
-        broker_address="localhost:9092",
-        loglevel="DEBUG",
-        consumer_group="music_events_processor",
-        auto_offset_reset="earliest",
-    )
+    broker_address="localhost:9092",
+    loglevel="DEBUG",
+    consumer_group="music_events_processor",
+    auto_offset_reset="earliest",
+)
+
+data_dict = {k: [] for k in list_of_topics}
 
 with app.get_consumer() as consumer:
-    msgs = []
-    while len(msgs) < 100000:
 
-        consumer.subscribe(topics=["listen_events"])
-        msg = consumer.poll(1)
-        #breakpoint()
-        if msg == None:
+    while len(data_dict["listen_events"]) < 100000:
+        consumer.subscribe(topics=list_of_topics)
+        msg = consumer.poll()
+        topic = msg.topic()
+        data_dict[topic].append(json.loads(msg.value().decode("utf-8")))
+
+        if not msg:
             break
-        msgs.append(json.loads(msg.value().decode('utf-8')))
-        
-df = pd.DataFrame(msgs)
-df.to_csv('events.csv')
+
+for topic, data in data_dict.items():
+    df = pd.DataFrame(data)
+    df.to_csv(f"data/{topic}.csv", index=False)
